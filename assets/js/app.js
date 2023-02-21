131c48acb272df36e52e483051d4f40873a390f1
TheMax8880 <maxime.trouvillers@gmail.com>
2023-02-21 14:13:27 +0100

    maj 20230221

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,12 @@ AOS.init({
     easing: 'ease-in-sine',
     disable: 'mobile'
 });
+
+window.onload = function() {
+    try {
+      TagCanvas.Start('myCanvas');
+    } catch(e) {
+      // something went wrong, hide the canvas container
+      document.getElementById('myCanvasContainer').style.display = 'none';
+    }
+  };
